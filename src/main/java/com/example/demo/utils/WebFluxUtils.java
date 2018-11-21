1205eef90a0b1311cafdce69147b1e0fb4f397b9
sevenlfanks <godjdc@gmail.com>
2018-11-21 12:35:04 +0800

    add html demo

--- src/main/java/com/example/demo/utils/WebFluxUtils.java
+++ src/main/java/com/example/demo/utils/WebFluxUtils.java
@@ -7,7 +7,10 @@ import reactor.core.publisher.Flux;
 public abstract class WebFluxUtils {
 
 	public static <T> Flux<ServerSentEvent<T>> SSE(Flux<T> flux) {
-		return flux.map(e -> ServerSentEvent.builder(e).build());
+		return flux.map(e -> ServerSentEvent.builder(e)
+//				.id(Long.toString(Instant.now().toEpochMilli()))
+//				.event("default")
+				.build());
 	}
 
 }
